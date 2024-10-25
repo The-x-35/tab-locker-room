a55d77e9c1a29bab1cdd95d634349dee82193589
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-10-26 02:38:37 +0530

    Relabbeled button to unlock

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ const App: React.FC = () => {
           className="password-input-main"
         />
         <button onClick={verifyPassword} className="password-submit-button">
-          Submit
+          Unlock
         </button>
       </div>
     );
